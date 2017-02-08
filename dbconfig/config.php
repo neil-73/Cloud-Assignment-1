261c8431e5de40f0118c84a9daffe835b0d691d6
Neil Ramdath <100519195@oncampus.local>
2017-02-08 00:03:28 -0500

    Add heroku db

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,4 +1,12 @@
 <?php
-$con = mysqli_connect("localhost","root","") or die("Unable to connect");
-mysqli_select_db($con,"samplelogindb")
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+//$con = mysqli_connect("localhost","root","") or die("Unable to connect");
+$con = mysqli_connect($server, $username, $password);
+mysqli_select_db($con,$db)
 ?>
